c8190c722ad86bc3e955d910e248a6bc561fa5c7
Ibad Khan <ik.ibadkhan@gmail.com>
2015-12-03 10:18:33 -0800

    Minor changes

--- eva.py
+++ eva.py
@@ -3,19 +3,29 @@ from bs4 import BeautifulSoup
 
 
 def get_source(url):
+    """
+    Read the html source from the url provided.
+    """
     return urllib2.urlopen(url).read()
 
 
 def get_tree(source):
+    """
+    Prepares the tree object out of html source
+    """
     return BeautifulSoup(source, 'html.parser')
 
 
-def get_tweets(tree):
+def print_tweets(tree):
+    """
+    Crawls through the twitter source and fetches tweets
+    """
     para = tree.findAll('p', {'class': 'TweetTextSize'})
     for p in para:
         print p.text
 
 
-source = get_source('https://twitter.com/thequote/')
-tree = get_tree(source)
-get_tweets(tree)
+if __name__ == '__main__':
+    source = get_source('https://twitter.com/thequote/')
+    tree = get_tree(source)
+    print_tweets(tree)
